13f714b0bc7966b3de1a7bfa4d6b8873835ea8d8
Felipe <fatamanczuk@degdigital.com>
2022-04-15 19:42:32 -0300

    Reinstall

--- ts-node/app.ts
+++
@@ -1,9 +0,0 @@
-import { appendFile } from "fs/promises";
-
-var data: string = 'Typescript Test'
-
-appendFile('my_file.txt', data).then((a) =>{
-    console.log('Arquivo criado ou atualizado com Sucesso!');
-}).catch((e) => {
-    console.log('Erro: ',e );
-});
