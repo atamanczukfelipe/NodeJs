62f5e883abf376360b64aa8579576b7d633dc8e1
Felipe <fatamanczuk@degdigital.com>
2022-04-15 10:40:51 -0300

    ts file example

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,9 +1,19 @@
 //First Ts Example
-var a: string = 'Test';
+/*var a: string = 'Test';
 var b: number = 123;
 
 function  example(numero: number, texto: string){
     console.log(texto, numero);
 }
 
-example(b,a);
+example(b,a);*/
+
+
+import * as fs from 'fs';
+
+fs.writeFile('arquivo.txt', 'Teste escrita de arquivo', (err)=>{
+    if (err){
+        console.log(err);
+    }
+    console.log('Arquivo Criado com Sucesso!');
+});
